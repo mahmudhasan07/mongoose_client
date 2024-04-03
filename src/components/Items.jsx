217c740519ed25ceba836bfcd2d4f1f140cf42ed
Mahmudhasan <mahmudhasan.hb@gmail.com>
2024-04-03 19:29:41 +0600

    try to solve framer motion

--- src/components/Items.jsx
+++ src/components/Items.jsx
@@ -1,5 +1,7 @@
 import axios from 'axios';
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useRef, useState } from 'react';
+import { motion } from "framer-motion"
+import { useInView } from "framer-motion"
 
 const Items = () => {
     const [array, setarray] = useState([]);
@@ -19,19 +21,21 @@ const Items = () => {
     return (
         <section>
             <h1 className='text-3xl text-center my-5'>Your Items</h1>
-            <div className='flex flex-wrap justify-center gap-10'>
+            <div className=' flex flex-wrap justify-center gap-16'>
                 {
                     loading == true ?
                         "loading"
                         :
-                        array.map(element => <Card key={element._id} setloading={setloading} card={element}></Card>)
+                        array.map((element,idx) => <Card key={element._id} setloading={setloading} id={idx} card={element}></Card>)
                 }
             </div>
         </section>
     );
 };
 
-const Card = ({ card, setloading }) => {
+const Card = ({ card, setloading, id }) => {
+    const ref = useRef(null)
+    const inView = useInView(ref)
 
     const handledelete = (id) => {
         // console.log(id);
@@ -47,15 +51,15 @@ const Card = ({ card, setloading }) => {
             })
     }
     return (
-        <div className='w-80 border-2 flex flex-col  h-72'>
-            <h1>Name: {card?.name}</h1>
-            <h1>price: {card?.price}</h1>
-            <h1>Details: {card?.details}</h1>
+        <motion.div  initial={{x:-500, opacity:0}} animate={{x:0, opacity:1}} transition={{duration:1.5, delay:id*1.5}} viewport={{once:false}} className='w-80 border-2 flex flex-col  h-full'>
+            <h1 className='text-4xl font-bold'>Name: {card?.name}</h1>
+            <h1 className='text-3xl font-bold'>price: {card?.price}</h1>
+            <h1 className='my-3 text-xl font-semibold'>Details: {card?.details}</h1>
             <div className='mt-auto mx-auto my-3'>
                 <button onClick={() => handledelete(card._id)} className='btn btn-success'>Delete</button>
             </div>
 
-        </div>
+        </motion.div>
     )
 }
 
